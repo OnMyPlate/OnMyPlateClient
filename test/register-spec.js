ae2e1aa1d6d12067890b651b3b2d77ecc8fa706f
cyurtbil <cenky92@gmail.com>
2014-12-05 16:21:36 -0500

    added login controller wihtout any functionality, refactored my register page test

--- test/register-spec.js
+++ test/register-spec.js
@@ -1,13 +1,19 @@
-describe('OnMyPlate registeration page', function() {
-  it('should register the user wiht appropriate inputs', function() {
+describe('OnMyPlate', function() {
 
-    browser.get('http://localhost:9000/#/register');
+  describe('registeration page', function() {
 
-    element(by.model('user.username')).sendKeys('realcritic');
-    element(by.model('user.email')).sendKeys('realcritic@example.com');
-    element(by.model('user.password')).sendKeys('test123');
-    element(by.model('user.password_confirmation')).sendKeys('test123');
+    beforeEach(function() {
+      browser.get('http://localhost:9000/#/register');
+    });
 
-    element(by.css('.btn.btn-default.register')).click();
+    it('should register the user wiht appropriate inputs', function() {
+
+      element(by.model('user.username')).sendKeys('realcritic');
+      element(by.model('user.email')).sendKeys('realcritic@example.com');
+      element(by.model('user.password')).sendKeys('test123');
+      element(by.model('user.password_confirmation')).sendKeys('test123');
+
+      element(by.css('.btn.btn-default.register')).click();
+    });
   });
 });
