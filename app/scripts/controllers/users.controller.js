8fca63f9e690070ceb86b78ed3d5aa4c96ba37e3
cyurtbil <cenky92@gmail.com>
2014-12-05 15:34:44 -0500

    added function registerUser and angular attributres to the form

--- app/scripts/controllers/users.controller.js
+++ app/scripts/controllers/users.controller.js
@@ -1,5 +1,17 @@
 'use strict';
 
 app.controller('UserCtrl', function($http, $scope, userFactory, ServerUrl) {
-  
+
+
+  // user can register
+
+  $scope.registerUser = function(user) {
+    var params = {user: user}
+
+    $http.post(ServerUrl + 'users', params).success(function(response) {
+      debugger
+      console.log(response)
+    });
+  }
+
 });
