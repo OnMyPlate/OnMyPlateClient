ce72b7be8b398bb4628a1c19dd35658342797527
cyurtbil <cenky92@gmail.com>
2014-12-04 22:45:01 -0500

    added logut function to the authfactory

--- app/scripts/factories/AuthFactory.js
+++ app/scripts/factories/AuthFactory.js
@@ -6,8 +6,16 @@ app.factory('AuthFactory', function($http, $window, ServerUrl) {
     return $http.post(ServerUrl + 'login', params).success(function(response) {
       // sessionStorage.setItem('key', 'value')
       $window.sessionStorage.setItem('OnMyPlate.user', response.token);
-      $http.defaults.headers.common['Autherization'] = 'Token token=' + $window.sessionStorage.getItem('OnMyPlate.user');
+
+      // Sets the headers for the request
+      $http.defaults.headers.common.Authorization = 'Token token=' + $window.sessionStorage.getItem('OnMyPlate.user');
     }); 
+  };
+
+  var logout = function() {
+    return $http.get(ServerUrl + '/logout').sucess(function(response) {
+      $window.sessionStorage.removeItem('OnMyPlate.user');
+    });
   };
 
 });
