2b6290759a547cfd7726d1abdf934da5e4bd31b1
cyurtbil <cenky92@gmail.com>
2014-12-15 12:12:33 -0500

    now food page can show the ratings and other credentials

--- app/scripts/controllers/food.controller.js
+++ app/scripts/controllers/food.controller.js
@@ -8,5 +8,16 @@ app.controller('FoodCtrl',['$location', '$scope', 'dataFactory', 'foodFactory', 
       var path = $location.path();
       $scope.currentFood = foodFactory.findCurrentFood(foods, path);
     });
-    
+
+    $scope.getRating = function(post) {
+      var repeat = [];
+      for(var i = 0; i < post.rating; i++) {
+        repeat.push(i);
+      }
+      return repeat;
+    };
+
+
+
+
 }]);
