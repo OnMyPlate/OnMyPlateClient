322415fdd418be8b1dbf34b5ad254ea3a85323e1
cyurtbil <cenky92@gmail.com>
2014-12-17 11:35:11 -0500

    now average food rating shows up in home oage

--- app/scripts/controllers/home.controller.js
+++ app/scripts/controllers/home.controller.js
@@ -1,9 +1,18 @@
 'use strict';
 
-app.controller('HomeCtrl', ['dataFactory', '$scope', function(dataFactory, $scope) {
+app.controller('HomeCtrl', ['dataFactory', '$scope', 'foodFactory', function(dataFactory, $scope, foodFactory) {
 
   dataFactory.fetchFoods().then(function(response) {
     $scope.foods = response.data.foods;
   });
+
+  $scope.getAvgRating = function(food) {
+    var posts = food.posts;
+
+    foodFactory.calcFoodRating(posts);
+    return foodFactory.ratingsArr;
+  };
+
+
 
 }]);
