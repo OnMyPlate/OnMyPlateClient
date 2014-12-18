bb3267b13801bbaadc633fc0fb84081669bbf8fb
cyurtbil <cenky92@gmail.com>
2014-12-17 19:50:58 -0500

    user can successfully bookmark, and fixed some bugs

--- app/scripts/controllers/favorite.controller.js
+++ app/scripts/controllers/favorite.controller.js
@@ -6,6 +6,6 @@ app.controller('FavoriteCtrl', ['dataFactory', '$scope', function(dataFactory, $
   dataFactory.fetchFoods().then(function(response) {
     $scope.foods = response.data.foods.filter(function(element) {
       return element.bookmarked === true;
-    })
+    });
   }); 
 }]);
