cd8c359ceba454237e35dd42baceec9bee3fa86c
cyurtbil <cenky92@gmail.com>
2014-12-17 21:07:16 -0500

    now users can unbookmark their bookmarked fooods

--- app/scripts/controllers/favorite.controller.js
+++ app/scripts/controllers/favorite.controller.js
@@ -1,11 +1,26 @@
 'use strict';
 
-app.controller('FavoriteCtrl', ['dataFactory', '$scope', function(dataFactory, $scope) {
+app.controller('FavoriteCtrl', ['dataFactory', 
+                                '$scope', 
+                                '$http', 
+                                'ServerUrl',
+                                function(dataFactory, $scope, $http, ServerUrl) {
 
 
   dataFactory.fetchFoods().then(function(response) {
     $scope.foods = response.data.foods.filter(function(element) {
       return element.bookmarked === true;
     });
-  }); 
+  });
+
+  $scope.bookmarkFood = function(food) {
+    food.bookmarked = !food.bookmarked
+    var params = {food: food};
+    $http.put(ServerUrl + 'foods/' + food.id + '.json', params).success(function(response) {
+      console.log('food bookmarked!');
+      $scope.foods = $scope.foods.filter(function(element) {
+        return element.bookmarked === true; 
+      });
+    });
+  }; 
 }]);
