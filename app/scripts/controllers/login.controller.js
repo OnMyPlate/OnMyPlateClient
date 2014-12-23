8ae5fa794f11e9083584e0f02e0af4b6a160cbfb
cyurtbil <cenky92@gmail.com>
2014-12-23 10:59:56 -0500

    fixed the smartphone button position issue, and added error message if user enters wrong email or password

--- app/scripts/controllers/login.controller.js
+++ app/scripts/controllers/login.controller.js
@@ -2,10 +2,16 @@
 
 app.controller('LoginCtrl',['$scope', '$location', 'authFactory', function($scope, $location, authFactory) {
 
+  $scope.isLoginSuccessful = true;
 
   $scope.login = function(params) {
     authFactory.login(params).success(function(response) {
-      $location.path('/')
+      $location.path('/');
+    }).error(function(response) {
+      $location.path('/login');
+      $scope.isLoginSuccessful = false;
+      $('#login-error').slideDown(200);
     });
   }
+
 }]);
