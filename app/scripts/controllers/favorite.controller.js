0a5a0b14d6aaaa8c9bf7abf22b95dcbae46cb677
cyurtbil <cenky92@gmail.com>
2014-12-24 13:51:08 -0500

    syncronized the asyncronous calls

--- app/scripts/controllers/favorite.controller.js
+++ app/scripts/controllers/favorite.controller.js
@@ -15,25 +15,31 @@ app.controller('FavoriteCtrl', ['dataFactory',
   dataFactory.fetchUsers().then(function(response) {
     $q.all(userFactory.createUsersArray(response.data.users, users)).then(function() {
       $scope.currentUser = userFactory.defineCurrentUser(users);
+      console.log($scope.currentUser);
+      $q.all(dataFactory.fetchFoods().then(function(response) {
+        $scope.foods = response.data.foods;
+        console.log($scope.foods);
+        $q.all(dataFactory.fetchBookmarks().then(function(response) {
+          $scope.bookmarks = response.data.bookmarks;
+          console.log($scope.bookmarks);
+        }));
+      }));
     });
   });
 
-  dataFactory.fetchFoods().then(function(response) {
-    $scope.foods = response.data.foods.filter(function(food) {
-      return food.bookmarked === true;
-    })
-    .filter(function(food) {return food.user_bookmarked === $scope.currentUser.id});
-  });
-
-  $scope.unBookmarkFood = function(food, user) {
+  $scope.unBookmarkFood = function(food, user, bookmarks) {
     debugger
-    $http.delete(HerokuUrl + 'bookmarks/' + bookmark.id + '.json', params).success(function(response) {
+    $http.delete(HerokuUrl + 'bookmarks/' + bookmark.id + '.json').success(function(response) {
       console.log('food unbookmarked!');
       $scope.foods = $scope.foods.filter(function(food) {
         return food.bookmarked === true; 
       });
     });
   }; 
+
+  $scope.isBookmarked = function(food, user, bookmarks) {
+    // console.log(bookmarks);
+  };
 
   $scope.getAvgRating = function(food) {
     var posts = food.posts;
