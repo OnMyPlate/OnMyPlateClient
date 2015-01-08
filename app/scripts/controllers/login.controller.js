61f3424b985da7311643df54c6578110b7674326
cyurtbil <cenky92@gmail.com>
2015-01-08 14:26:56 -0500

    fized uathorization setting on user login

--- app/scripts/controllers/login.controller.js
+++ app/scripts/controllers/login.controller.js
@@ -1,6 +1,12 @@
 'use strict';
 
-app.controller('LoginCtrl',['$scope', '$location', 'authFactory', 'dataFactory', function($scope, $location, authFactory, dataFactory) {
+app.controller('LoginCtrl',['$scope', 
+                            '$location', 
+                            'authFactory', 
+                            'dataFactory', 
+                            '$window',
+                            '$http',
+                             function($scope, $location, authFactory, dataFactory, $window, $http) {
 
   $scope.isLoginSuccessful = true;
   $scope.isConfirmed = true;
@@ -9,6 +15,9 @@ app.controller('LoginCtrl',['$scope', '$location', 'authFactory', 'dataFactory',
     dataFactory.getConfirm().then(function(response) {
       if(response.data.confirmed) {
         authFactory.login(params).success(function(response) {
+          $window.sessionStorage.setItem('OnMyPlate.user', response.token);
+          // Sets the headers for the request, and token for the authorization
+          $http.defaults.headers.common['Authorization'] = 'Token token=' + $window.sessionStorage.getItem('OnMyPlate.user');
           $location.path('/');
         }).error(function(response) {
           $location.path('/login');
